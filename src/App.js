4b341a06a06549e176bc84d1ab29510ab7ce44eb
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-03-31 19:35:27 +0200

    Switched method to config and main logic modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import config from './config';
 
-function App() {
+function Asts() {
+  const [asteroids, setAsteroids] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${config.NASA_API_KEY}`)
+      .then(response => {
+        setAsteroids(response.data.near_earth_objects);
+      })
+      .catch(error => {
+        console.error('Error fetching API:', error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Neows API</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Distance from Earth (km)</th>
+            <th>Orbiting Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {asteroids.map(asteroid => (
+            <tr key={asteroid.id}>
+              <td>{asteroid.name}</td>
+              <td>{Math.round(asteroid.close_approach_data[0].miss_distance.kilometers)}</td>
+              <td>{asteroid.close_approach_data[0].orbiting_body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
 
-export default App;
+export default Asts;
