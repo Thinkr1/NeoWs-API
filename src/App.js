61ac08fee8a03231c2a141754a1e3e0e9b69a7d8
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-04-01 12:12:34 +0200

    ID and orbiting body

--- src/App.js
+++ src/App.js
@@ -35,13 +35,13 @@ function Asts() {
 
   return (
     <div className='container'>
-      <h1>NeoWs API</h1>
+      <h1>NeoWs NASA API</h1>
       <table className='table'>
         <thead>
           <tr>
+            <th>ID</th>
             <th>Name</th>
             <th>Estimated diameter (km)</th>
-            <th>Orbiting Body</th>
             {/* <th>Distance from Earth (km)</th> */}
             <th>Potentially Hazardous?</th>
             <th>First recorded approach</th>
@@ -53,14 +53,14 @@ function Asts() {
         <tbody>
           {asteroids.map(asteroid => (
             <tr key={asteroid.id}>
+              <td>{asteroid.id}</td> {/*ID*/}
               <td><a href={asteroid.nasa_jpl_url} target="_blank" rel="noopener noreferrer">{asteroid.name}</a></td> {/*Name + link*/}
               <td>{(((asteroid.estimated_diameter.kilometers.estimated_diameter_min)+(asteroid.estimated_diameter.kilometers.estimated_diameter_max))/2).toFixed(3)}</td>  {/*Diameter*/}
-              <td>{asteroid.close_approach_data[0].orbiting_body}</td>  {/*Orbiting body*/}
               {/* <td>{Math.round(asteroid.close_approach_data[0].miss_distance.kilometers)}</td> */}  {/*Distance from Earth in first recorded approach*/}
               <td>{asteroid.is_potentially_hazardous_asteroid? "Yes" : "No"}</td>  {/*Hazardous?*/}
-              <td>{asteroid.close_approach_data[0].close_approach_date}</td> {/*First recorded approach*/}
+              <td>{asteroid.close_approach_data[0].close_approach_date} (orbiting {asteroid.close_approach_data[0].orbiting_body})</td> {/*First recorded approach*/}
               <td>{Math.round(asteroid.close_approach_data[0].relative_velocity.kilometers_per_hour)}</td> {/*First recorded approach relative velocity*/}
-              <td>{getLastApproach(asteroid.close_approach_data).close_approach_date}</td> {/*Last recorded approach*/}
+              <td>{getLastApproach(asteroid.close_approach_data).close_approach_date} (orbiting {getLastApproach(asteroid.close_approach_data).orbiting_body})</td> {/*Last recorded approach*/}
               <td>{Math.round(getLastApproach(asteroid.close_approach_data).relative_velocity.kilometers_per_hour)}</td> {/*Last recorded approach relative velocity*/}
             </tr>
           ))}
