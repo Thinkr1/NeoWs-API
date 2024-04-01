aa88beb72ee2609d9341710b02c53f90ac88528a
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-04-01 13:13:32 +0200

    Orbital period, first and last observations

--- src/App.js
+++ src/App.js
@@ -44,6 +44,9 @@ function Asts() {
             <th>Estimated diameter (km)</th>
             {/* <th>Distance from Earth (km)</th> */}
             <th>Potentially Hazardous?</th>
+            <th>Orbital period (Earth Days)</th>
+            <th>First Observation</th>
+            <th>Last Observation</th>
             <th>First recorded approach</th>
             <th>First recorded approach relative velocity (km/h)</th>
             <th>Last recorded approach</th>
@@ -58,6 +61,9 @@ function Asts() {
               <td>{(((asteroid.estimated_diameter.kilometers.estimated_diameter_min)+(asteroid.estimated_diameter.kilometers.estimated_diameter_max))/2).toFixed(3)}</td>  {/*Diameter*/}
               {/* <td>{Math.round(asteroid.close_approach_data[0].miss_distance.kilometers)}</td> */}  {/*Distance from Earth in first recorded approach*/}
               <td>{asteroid.is_potentially_hazardous_asteroid? "Yes" : "No"}</td>  {/*Hazardous?*/}
+              <td>{Math.round(asteroid.orbital_data.orbital_period * 10) / 10}</td> {/*Orbital period*/}
+              <td>{asteroid.orbital_data.first_observation_date}</td> {/*First observation*/}
+              <td>{asteroid.orbital_data.last_observation_date}</td> {/*Last observation*/}
               <td>{asteroid.close_approach_data[0].close_approach_date} (orbiting {asteroid.close_approach_data[0].orbiting_body})</td> {/*First recorded approach*/}
               <td>{Math.round(asteroid.close_approach_data[0].relative_velocity.kilometers_per_hour)}</td> {/*First recorded approach relative velocity*/}
               <td>{getLastApproach(asteroid.close_approach_data).close_approach_date} (orbiting {getLastApproach(asteroid.close_approach_data).orbiting_body})</td> {/*Last recorded approach*/}
