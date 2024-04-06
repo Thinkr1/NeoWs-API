f8c93e55dc09a9343e8e4fc972e0e3baf62f3162
Thinkr1 <146010473+Thinkr1@users.noreply.github.com>
2024-04-06 12:31:56 +0200

    Formatting

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./styles.css";
 function Asts() {
   const [asteroids, setAsteroids] = useState([]);
   const [approachData, setApproachData] = useState([]);
-  const [astID, setAstID] = useState(0)
+  const [astID, setAstID] = useState(0);
 
   useEffect(() => {
     axios
@@ -37,17 +37,19 @@ function Asts() {
   };
 
   const getAstInfo = (id) => {
-    setAstID(id)
+    setAstID(id);
     const ast = asteroids.find((asteroid) => asteroid.id === id);
     if (ast) {
       axios
-        .get(`https://api.nasa.gov/neo/rest/v1/neo/${id}?api_key=${config.NASA_API_KEY}`)
+        .get(
+          `https://api.nasa.gov/neo/rest/v1/neo/${id}?api_key=${config.NASA_API_KEY}`
+        )
         .then((response) => {
           setApproachData(response.data.close_approach_data);
         })
         .catch((error) => {
-          console.error("Error fetching API: ", error)
-        })
+          console.error("Error fetching API: ", error);
+        });
     } else {
       console.log("Asteroid not found");
     }
@@ -56,7 +58,7 @@ function Asts() {
   return (
     <div className="container">
       <h1>NeoWs NASA API</h1>
-      <table className='table'>
+      <table className="table">
         <thead>
           <tr>
             <th>ID</th>
@@ -74,25 +76,84 @@ function Asts() {
           </tr>
         </thead>
         <tbody>
-        {asteroids.map(asteroid => (
-          <tr key={asteroid.id}>
-            <td><a href={asteroid.nasa_jpl_url} target="_blank" rel="noopener noreferrer">{asteroid.id}</a></td>{/* ID */}
-            <td><a href={asteroid.nasa_jpl_url} target="_blank" rel="noopener noreferrer">{asteroid.name}</a></td>{/* Name + link */}
-            <td>{(((asteroid.estimated_diameter.kilometers.estimated_diameter_min)+(asteroid.estimated_diameter.kilometers.estimated_diameter_max))/2).toFixed(3)}</td>{/* Diameter */}
-            <td>{asteroid.is_potentially_hazardous_asteroid? "Yes" : "No"}</td>{/* Hazardous? */}
-            <td>{Math.round(asteroid.orbital_data.orbital_period * 10) / 10}</td>{/* Orbital period */}
-            <td>{asteroid.orbital_data.first_observation_date}</td>{/* First observation */}
-            <td>{asteroid.orbital_data.last_observation_date}</td>{/* Last observation */}
-            <td>{asteroid.close_approach_data[0].close_approach_date} (orbiting {asteroid.close_approach_data[0].orbiting_body})</td>{/* First recorded approach */}
-            <td>{Math.round(asteroid.close_approach_data[0].relative_velocity.kilometers_per_hour)}</td>{/* First recorded approach relative velocity */}
-            <td>{getLastApproach(asteroid.close_approach_data).close_approach_date} (orbiting {getLastApproach(asteroid.close_approach_data).orbiting_body})</td>{/* Last recorded approach */}
-            <td>{Math.round(getLastApproach(asteroid.close_approach_data).relative_velocity.kilometers_per_hour)}</td>{/* Last recorded approach relative velocity */}
-          </tr>
-        ))}
+          {asteroids.map((asteroid) => (
+            <tr key={asteroid.id}>
+              <td>
+                <a
+                  href={asteroid.nasa_jpl_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {asteroid.id}
+                </a>
+              </td>
+              {/* ID */}
+              <td>
+                <a
+                  href={asteroid.nasa_jpl_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {asteroid.name}
+                </a>
+              </td>
+              {/* Name + link */}
+              <td>
+                {(
+                  (asteroid.estimated_diameter.kilometers
+                    .estimated_diameter_min +
+                    asteroid.estimated_diameter.kilometers
+                      .estimated_diameter_max) /
+                  2
+                ).toFixed(3)}
+              </td>
+              {/* Diameter */}
+              <td>
+                {asteroid.is_potentially_hazardous_asteroid ? "Yes" : "No"}
+              </td>
+              {/* Hazardous? */}
+              <td>
+                {Math.round(asteroid.orbital_data.orbital_period * 10) / 10}
+              </td>
+              {/* Orbital period */}
+              <td>{asteroid.orbital_data.first_observation_date}</td>
+              {/* First observation */}
+              <td>{asteroid.orbital_data.last_observation_date}</td>
+              {/* Last observation */}
+              <td>
+                {asteroid.close_approach_data[0].close_approach_date} (orbiting{" "}
+                {asteroid.close_approach_data[0].orbiting_body})
+              </td>
+              {/* First recorded approach */}
+              <td>
+                {Math.round(
+                  asteroid.close_approach_data[0].relative_velocity
+                    .kilometers_per_hour
+                )}
+              </td>
+              {/* First recorded approach relative velocity */}
+              <td>
+                {
+                  getLastApproach(asteroid.close_approach_data)
+                    .close_approach_date
+                }{" "}
+                (orbiting{" "}
+                {getLastApproach(asteroid.close_approach_data).orbiting_body})
+              </td>
+              {/* Last recorded approach */}
+              <td>
+                {Math.round(
+                  getLastApproach(asteroid.close_approach_data)
+                    .relative_velocity.kilometers_per_hour
+                )}
+              </td>
+              {/* Last recorded approach relative velocity */}
+            </tr>
+          ))}
         </tbody>
       </table>
       <br />
-      <hr/>
+      <hr />
       <br />
       <h1>Search by ID</h1>
       <div className="centered-div">
@@ -105,7 +166,15 @@ function Asts() {
       </div>
       <br />
       <br />
-      <h2><a href={asteroids.filter(ast => ast.id === astID)[0]?.nasa_jpl_url} target="_blank" rel="noopener noreferrer">{asteroids.filter(ast => ast.id === astID)[0]?.name}</a></h2>
+      <h2>
+        <a
+          href={asteroids.filter((ast) => ast.id === astID)[0]?.nasa_jpl_url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {asteroids.filter((ast) => ast.id === astID)[0]?.name}
+        </a>
+      </h2>
       <table className="search-table">
         <thead>
           <tr>
@@ -119,7 +188,7 @@ function Asts() {
         <tbody>
           {approachData.map((entry, index) => (
             <tr key={index}>
-              <td>{index+1}</td>
+              <td>{index + 1}</td>
               <td>{entry.close_approach_date}</td>
               <td>{Math.round(entry.relative_velocity.kilometers_per_hour)}</td>
               <td>{Math.round(entry.miss_distance.kilometers)}</td>
